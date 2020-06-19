41f6786b90b5bc3b0ed9169407843f6600aadcfc
DasH <fxfddfno@gmail.com>
2020-06-19 21:10:02 +0300

    get the more word finded using tuples

--- ex_02/code.py
+++ ex_02/code.py
@@ -23,3 +23,22 @@ for key in di:
 
 
 print(theWord, maxValue)
+
+# Using Tuples
+
+newFile = open("./clown.txt")
+
+newDict = dict()
+for line in newFile:
+    words = line.split()
+    for word in words:
+        x = newDict.get(word)
+        newDict[word] = newDict.get(word, 0) + 1
+
+lst = list()
+for k, v in newDict.items():
+    lst.append((v, k))
+
+lst = sorted(lst, reverse=True)
+for v, k in lst:
+    print(v, k)
