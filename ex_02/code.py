2bfe071f9e994d161880c5a14d2c0095e7d77da9
DasH <fxfddfno@gmail.com>
2020-06-19 19:23:04 +0300

    improve code

--- ex_02/code.py
+++ ex_02/code.py
@@ -10,9 +10,7 @@ for line in hand:
     line = line.rstrip()
     line = line.split()
     for word in line:
-        if word in di:
-            di[word] = di[word] + 1
-        else:
-            di[word] = 1
+        count = di.get(word, 0) + 1
+        di[word] = count
 
 print(di)
