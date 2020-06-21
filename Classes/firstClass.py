0d7a3ef3434d6d826a5b202720d02268c78fc5f1
DasH <fxfddfno@gmail.com>
2020-06-21 16:30:23 +0300

    add constructor to class using new object

--- Classes/firstClass.py
+++ Classes/firstClass.py
@@ -18,3 +18,23 @@ an.party()
 an = 542
 
 print(" an contains", an)
+
+# another class
+print("another class")
+
+
+class PrintNames:
+    x = 0
+    name = ''
+
+    def __init__(self, z):
+        self.name = z
+        print(self.name, "constructed")
+
+    def addCount(self):
+        self.x = self.x + 1
+        print(self.x, "Count for name", self.name)
+
+
+morad = PrintNames("morad")
+morad.addCount()
