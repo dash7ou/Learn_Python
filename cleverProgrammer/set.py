7286f292c7c35f0cc42c48a2a63227081baa5416
DasH <fxfddfno@gmail.com>
2020-06-21 22:00:41 +0300

    set methods

--- cleverProgrammer/set.py
+++ cleverProgrammer/set.py
@@ -4,3 +4,12 @@ print(newSet)
 # add to set
 newSet.add("apple")
 print(newSet)
+
+# set in python using in digram => two digram share in area
+persons = {"morad", "noor", "shimaa"}
+anotherPersons = {"shimaa", "hadeel"}
+
+allPersons = persons.union(anotherPersons)
+personOfBoth = persons.intersection(anotherPersons)
+print(allPersons)
+print(personOfBoth)
