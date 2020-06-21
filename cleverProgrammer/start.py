7d7d95aca24da27aa7bc74523c2a125da7cdba91
DasH <fxfddfno@gmail.com>
2020-06-21 20:43:28 +0300

    deal with calender

---
+++ cleverProgrammer/start.py
@@ -0,0 +1,22 @@
+import calendar
+
+# 2 => number of character you want to show it :)
+print(calendar.weekheader(2))
+print()
+
+print(calendar.month(2020, 6))
+print()
+
+# print list of days every item as list 7 days
+print(calendar.monthcalendar(2020, 6))
+print()
+
+# print year calender
+print(calendar.calendar(2020))
+
+
+dayOfTheWeek = calendar.weekday(2020, 6, 20)
+print(dayOfTheWeek)
+
+isLeap = calendar.isleap(2020)
+print(isLeap)
